9885a09b1e36fcd9c81645f7014de9a17447f730
bsnsshan8-svg <bsnsshan8@gmail.com>
2026-09-21 04:19:56 +0700

    Route clinic to its dedicated clinic page

--- src/routes/clinic.tsx
+++ src/routes/clinic.tsx
@@ -2,5 +2,5 @@ import { createFileRoute } from "@tanstack/react-router";
 import App from "../App.jsx";
 
 export const Route = createFileRoute("/clinic")({
-  component: App,
+  component: () => <App page="clinic" />,
 });
