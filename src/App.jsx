102d079990c09a449a2a067c2b1400f054cc375b
bsnsshan8-svg <bsnsshan8@gmail.com>
2026-09-21 02:26:26 +0700

    Make clinic page production-safe by loading global CSS from the root only

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import "./index.css";
-import { ArrowRight, Check, ChevronDown, Instagram, Menu, Sparkles, Star, X } from "lucide-react";
+import { ArrowRight, Check, Instagram, Menu, Sparkles, Star, X } from "lucide-react";
 
 const instagramUrl = "https://www.instagram.com/drnadahassan/?hl=en";
 
